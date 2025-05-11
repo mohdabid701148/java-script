45ac92794dc1ff8a7dee66594d7b63325959b2a0
mohdabid701148 <abidraeen05@gmail.com>
2025-05-11 20:20:46 +0000

    learn data and variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 144556
+let accountEmail = "abidrayeen05@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState ;
+// accountId = 1 //not allowed
+accountEmail = "mohdabid@gmail.com"
+accountPassword = "701148"
+accountCity = "Delhi"
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treate all js as newer version
+
+//alert(3+3) // we are using node.js not browser
+
+
+let name = "Abid"
+let age = 19
+let IsLoggedIn = false
+let state = null
+
+// number =>2 to power 53
+// BigInt
+// string => ""
+//boolean 
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+//object
+console.log( typeof "Abid")
+console.log( typeof age)
+console.log( typeof null) // object
+// null is an object data type
