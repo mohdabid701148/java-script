b519a6ad3a3605468e319f9dedcd449d7b8f5ee8
mohdabid701148 <abidraeen05@gmail.com>
2025-05-12 06:43:53 +0000

    comparesion

---
+++ 01_basics/04_comparesion.js
@@ -0,0 +1,17 @@
+// console.log(2>3)
+// console.log("2">1)
+// console.log("02">1)
+
+
+// console.log(null==0)
+// console.log(null>0)
+// console.log(null>=0)
+           
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined>=0)
+//avoid above comparision
+
+// === strict check it also check data type
+console.log("2"==2) // true
+console.log("2"===2) //false
